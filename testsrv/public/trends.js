7586d672a21cdd3a938b2109e3ce84098ce4e354
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2023-01-30 16:08:00 +0100

    small fixes

--- testsrv/public/trends.js
+++ testsrv/public/trends.js
@@ -9,10 +9,10 @@ const readTrends = async () =>{
   const response = await fetch('/google-trends')
   const trends = await response.json()  
   let html = ''
+  console.log(trends)
   trends.results.forEach(trend => {
     html += `<p><b>${trend.Top_Term}</b> (${trend.Day.value})</p>`
   })
-  console.log(JSON.stringify(trends))
   const resultsDiv = document.getElementById('trends-results')
   resultsDiv.innerHTML = html
 }
